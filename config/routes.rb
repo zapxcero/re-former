64bd63f008c695540ae18eb1f521cdd4065cdcae
Adrian Rodriguez <zapxcero@gmail.com>
2022-11-05 23:19:38 +0800

    #Editing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
 end
